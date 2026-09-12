6f984bdf6e69ea18a21883db9654cf70ef7de2c2
jaadu20 <jawadfarid383@gmail.com>
2026-09-12 21:43:19 +0500

    Add admin.py

---
+++ apps/users/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from apps.users.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'created_at')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name', 'phone_number')
+    ordering = ('-created_at',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined', 'created_at', 'updated_at')}),
+    )
+    readonly_fields = ('date_joined', 'created_at', 'updated_at')
