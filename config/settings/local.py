066557954aedf8185dd376b4005b88dd524353ee
jaadu20 <jawadfarid383@gmail.com>
2026-09-16 09:34:44 +0500

    Add config/settings/local.py

---
+++ config/settings/local.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Additional local development tools if installed
+try:
+    import debug_toolbar
+    INSTALLED_APPS += ['debug_toolbar']
+    MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
+    INTERNAL_IPS = ['127.0.0.1']
+except ImportError:
+    pass
