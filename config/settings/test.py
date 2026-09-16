e9350e9471708dc683786e460ecb6a22ab315158
jaadu20 <jawadfarid383@gmail.com>
2026-09-16 09:34:54 +0500

    Add config/settings/test.py

---
+++ config/settings/test.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = False
+TESTING = True
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
